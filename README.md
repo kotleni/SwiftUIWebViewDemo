189b3a60665635e31b2022cd6da603be5e2a76e5
kotleni <yavarenikya@gmail.com>
2023-11-27 14:32:39 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<b>Basic SwiftUI camera demo</b><br>
+In this example implemented wrapped ```WKWebView``` using ```UIViewControllerRepresentable```.
+
+### Tested features
+Downloading, uploading, camera, microphone, JS.
--- WebViewDemo/ContentView.swift
+++ WebViewDemo/ContentView.swift
@@ -6,19 +6,50 @@
 //
 
 import SwiftUI
+import WebKit
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+class WebViewController: UIViewController, WKUIDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        super.loadView()
+        
+        let webConfiguration = WKWebViewConfiguration()
+        webConfiguration.ignoresViewportScaleLimits = true
+        webConfiguration.suppressesIncrementalRendering = true
+        webConfiguration.allowsInlineMediaPlayback = true
+        webConfiguration.allowsAirPlayForMediaPlayback = false
+        webConfiguration.allowsPictureInPictureMediaPlayback = true
+        webConfiguration.mediaTypesRequiringUserActionForPlayback = []
+        webConfiguration.requiresUserActionForMediaPlayback = false
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        //webView.navigationDelegate = self
+        webView.sizeToFit()
+        webView.isOpaque = false
+        view = webView
     }
 }
 
-#Preview {
-    ContentView()
+struct WebView: UIViewControllerRepresentable {
+    let url: URL
+    
+    func makeUIViewController(context: Context) -> WebViewController {
+        return WebViewController()
+    }
+    
+    func updateUIViewController(_ webViewController: WebViewController, context: Context) {
+        let request = URLRequest(url: url)
+        webViewController.webView.load(request)
+    }
 }
+
+struct ContentView: View {
+    var body: some View {
+        WebView(url: URL(string: "https://google.com")!)
+    }
+}
+
+//#Preview {
+//    ContentView()
+//}
