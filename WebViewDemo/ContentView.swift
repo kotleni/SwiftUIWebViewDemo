632f8138edd42224bf3570180693714b5cf83fd8
kotleni <yavarenikya@gmail.com>
2023-11-27 13:00:47 +0200

    Initial Commit

---
+++ WebViewDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WebViewDemo
+//
+//  Created by Victor Varenik on 27.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WebViewDemo/WebViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewDemoApp.swift
+//  WebViewDemo
+//
+//  Created by Victor Varenik on 27.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct WebViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
